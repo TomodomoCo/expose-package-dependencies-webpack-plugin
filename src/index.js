3517f10d9e4f2d1ccb2b00cd13dd0f7f97becc95
Chris Van Patten <hello@chrisvanpatten.com>
2019-12-13 14:54:32 -0500

    Support WP's new asset file format (#1)
    * attempt to support core's new asset file format
    
    * support the file structure change
    
    * streamline the logic and fix some bugs

--- src/index.js
+++ src/index.js
@@ -1,66 +1,92 @@
 const { RawSource } = require( 'webpack-sources' );
-const { union } = require( 'lodash' );
+const { find, findKey, get, union } = require( 'lodash' );
 
 class ExposePackageDependenciesWebpackPlugin {
-	constructor( options ) {
-		this.options = options;
-	}
+  constructor( options ) {
+    this.options = options;
+  }
 
-	apply( compiler ) {
-		const { output } = compiler.options;
-		const { filename: outputFilename } = output;
+  apply( compiler ) {
+    const { output } = compiler.options;
+    const { filename: outputFilename } = output;
 
-		compiler.hooks.emit.tap( this.constructor.name, ( compilation ) => {
-			// Iterate on the entrypoints (there is probably just one).
-			for ( const [ entrypointName, entrypoint ] of compilation.entrypoints.entries() ) {
-				const [ filename, query ] = entrypointName.split( '?', 2 );
+    compiler.hooks.emit.tap( this.constructor.name, ( compilation ) => {
+      // Iterate on the entrypoints (there is probably just one).
+      for ( const [ entrypointName, entrypoint ] of compilation.entrypoints.entries() ) {
+        const [ filename, query ] = entrypointName.split( '?', 2 );
 
-				// Get the name of the dependency file.
-				const depsFilename = compilation.getPath( outputFilename, {
-					chunk: entrypoint.getRuntimeChunk(),
-					filename,
-					query,
-					basename: basename( filename ),
-				} ).replace( /\.js$/i, '.deps.json' );
+        // Get the name of the dependency file.
+        const buildFilename = compilation.getPath( outputFilename, {
+          chunk: entrypoint.getRuntimeChunk(),
+          filename,
+          query,
+          basename: basename( filename ),
+        } );
 
-				const asset = compilation.assets[ depsFilename ] || null;
+        const extensions = [
+          '.asset.json',
+          '.deps.json',
+        ];
 
-				// If the deps.json file does not exist, stop here.
-				if ( asset === null ) {
-					return;
-				}
+        // Check if the build file has a matching asset file. If so, get the file.
+        const assetFiles = extensions.map( ( ext ) => buildFilename.replace( /\.js$/i, ext ) );
+        const assetFileName = findKey( compilation.assets, ( value, key ) => assetFiles.includes( key ) );
+        const asset = get( compilation.assets, assetFileName, null );
 
-				// Parse the existing source file, so it can be modified.
-				let deps = JSON.parse( asset.source() );
+        if ( asset === null ) {
+          return;
+        }
 
-				// Loop over the specified packages.
-				this.options.packages.forEach( ( packageName ) => {
-					// Get the package's deps.json file.
-					const packageData = require( packageName + '/package.json' );
-					const packageDepsFilename = packageData.main.replace( /\.js$/i, '.deps.json' );
-					const packageDeps = require( `${packageName}/${packageDepsFilename}` );
+        // Parse the existing source file, so it can be modified.
+        let deps = JSON.parse( asset.source() );
 
-					if ( ! Array.isArray( packageDeps ) ) {
-						return;
-					}
+        // Loop over the specified packages.
+        this.options.packages.forEach( ( packageName ) => {
+          let packageDependencies = null;
 
-					// Merge the package dependencies into the consumer's dependencies.
-					deps = union( deps, packageDeps );
-				} );
+          try {
+            // Get the list of possible asset files.
+            const packageData = require( packageName + '/package.json' );
+            const packageAssetFiles = extensions.map( ( ext ) => packageData.main.replace( /\.js$/i, ext ) );
 
-				// Output the result back to the dependency file.
-				compilation.assets[ depsFilename ] = new RawSource( JSON.stringify( deps ) );
-			}
-		} );
-	}
+            // Attempt to get the dependencies from each possible asset file.
+            const assetFiles = packageAssetFiles.map( ( filename ) => {
+              try {
+                const assetFile = require( `${packageName}/${filename}` );
+
+                return 'dependencies' in assetFile ? assetFile.dependencies : assetFile;
+              } catch ( e ) {
+                return false;
+              }
+            } );
+
+            // If we were able to find any dependencies, get them.
+            packageDependencies = find( assetFiles, ( item ) => !! item.length ) || null;
+          } catch ( e ) {
+            return;
+          }
+
+          if ( packageDependencies === null ) {
+            return;
+          }
+
+          // Merge the package's dependencies into the consumer's dependencies.
+          deps = union( deps, packageDependencies );
+        } );
+
+        // Output the result back to the dependency file.
+        compilation.assets[ assetFileName ] = new RawSource( JSON.stringify( deps ) );
+      }
+    } );
+  }
 }
 
 function basename( name ) {
-	if ( ! name.includes( '/' ) ) {
-		return name;
-	}
+  if ( ! name.includes( '/' ) ) {
+    return name;
+  }
 
-	return name.substr( name.lastIndexOf( '/' ) + 1 );
+  return name.substr( name.lastIndexOf( '/' ) + 1 );
 }
 
 module.exports = ExposePackageDependenciesWebpackPlugin;
